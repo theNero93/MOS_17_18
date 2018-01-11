213162f4077ef03429d15557852dd999c2416f55
schima <Martin.Schicklgruber@solvistas.com>
2018-01-11 09:32:06 +0100

    iOS Project

---
+++ MOS_Project_iOS/MOS_Project_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MOS_Project_iOS
+//
+//  Created by solvistas on 11.01.18.
+//  Copyright © 2018 fhooe.mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MOS_Project_iOS/Pods/Target Support Files/Pods-MOS_Project_iOS/Pods-MOS_Project_iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MOS_Project_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_MOS_Project_iOS
+@end
