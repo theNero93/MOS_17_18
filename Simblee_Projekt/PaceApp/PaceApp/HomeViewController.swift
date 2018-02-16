0e2dd2f4650f454e529d5d5b85f15efbe4111fe0
Nora Wokatsch <nora.wokatsch@students.fh-hagenberg.at>
2018-02-16 22:49:44 +0100

    finished UI added simblee Project as its own project, because of the account signing in xcode, i couldn't test it on my iphone, so i had to make another project

---
+++ Simblee_Projekt/PaceApp/PaceApp/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PaceApp
+//
+//  Created by Nora Isabel Wokatsch on 26.01.18.
+//  Copyright © 2018 Nora Isabel Wokatsch. All rights reserved.
+//
+
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.hidesBackButton = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
